fb0202b2a1c965705d81ac228ff9e375e100802c
Piotr Wójcik <chocimier@tlen.pl>
2019-04-05 10:45:23 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+tools_necessary="
+git
+rcup
+"
+
+error=
+for i in $tools_necessary; do
+	if ! command -v $i > /dev/null; then
+		echo "$i is necessary. Install it first."
+		error=1
+	fi
+done
+if [ "$error" ]; then
+	exit 1
+fi
+
+if ! [ -d ~/.oh-my-zsh ]; then
+	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+if ! [ -d ~/.oh-my-zsh/custom/plugins/zsh-completions ]; then
+	git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+fi
+if ! [ -d ~/.tacklebox ]; then
+	git clone https://github.com/justinmayer/tacklebox ~/.tacklebox
+fi
+if ! [ -d ~/.tackle ]; then
+	git clone https://github.com/justinmayer/tackle ~/.tackle
+fi
+
+# find directory
+cd "$(dirname $0)"
+dotfiles="$(pwd)"
+cd - > /dev/null
+
+RCRC="$dotfiles/rcrc" rcup -d "$dotfiles"
